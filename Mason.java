33b712b6ee208a2ee31b124177ecd8cf09a8468c
AlvaroFPCadiz <alvaro.vidal.canas.alu@iesfernandoaguilar.es>
2024-03-11 13:15:43 +0100

    Segundo comit después de la clonación

--- Mason.java
+++ Mason.java
@@ -2,5 +2,7 @@ public class Mason {
 	public static void main (String[] args) {
 		System.out.println("Masón");
 		System.out.println("Prueba antes de clonación");
+
+		System.out.println("Prueba después de la clonación");
 	}
 }
